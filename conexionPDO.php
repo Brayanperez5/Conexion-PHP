d162f500a3a21ebaba4c945eac1cbdebacd04dd6
Brayanperez5 <163586839+Brayanperez5@users.noreply.github.com>
2024-11-28 17:53:17 -0500

    Conexion
    Agregacion conexion

---
+++ conexionPDO.php
@@ -0,0 +1,33 @@
+<?php
+class Database
+{
+  private $servidorlocal;
+  private $basededatos;
+  private $usuario;
+  private $password;
+  private $caracteres;
+    function connectar()
+      {
+      $servidorlocal = 'localhost';
+      $basededatos   = 'crearticol_db';
+      $usuario       = 'root';
+      $password      = '';
+      $caracteres    = 'utf8';
+        try
+          {
+            $conexion = "mysql:host=".$servidorlocal.";dbname=".$basededatos.";charset=".$caracteres;
+            $opciones = [
+                          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                          PDO::ATTR_EMULATE_PREPARES  => false
+                        ];
+            $pdo = new PDO($conexion, $usuario, $password, $opciones);
+            return $pdo;
+          }
+        catch(PDOException $error)
+          {
+            echo 'Error en la conexion:  '.$error->getMessage();
+          }
+      }
+}
+//conexion::connect();
+?>
